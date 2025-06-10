f3d92ac3acc0d2c6b6fec495f5367db9f50cea2d
Erick1506 <163191519+Erick1506@users.noreply.github.com>
2025-06-10 12:15:09 -0500

    congiguraciondespliegue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,35 +3,29 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\View;
-use App\Models\Categoria;
-
-
-
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\URL;
+use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot()
+    public function boot(Request $request): void
     {
-        View::composer('layouts.menu', function ($view) {
-                logger('Se está ejecutando el View Composer del menú');
+        if ($this->app->environment('production')) {
+           $request->setTrustedProxies(
+                [$request->getClientIp()],
+                SymfonyRequest::HEADER_X_FORWARDED_FOR |
+                SymfonyRequest::HEADER_X_FORWARDED_HOST |
+                SymfonyRequest::HEADER_X_FORWARDED_PORT |
+                SymfonyRequest::HEADER_X_FORWARDED_PROTO
+            );
 
-        $view->with('categorias', Categoria::with('clasificaciones')->get());
-    });
+            URL::forceScheme('https');
+        }
     }
-
-    
 }
-
